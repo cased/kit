e400b35060e41a7b4d19bfb5ccf3595d4fb2df32
tnm <t@ted.io>
2025-06-14 12:15:46 -0700

    Add demo test file v1

---
+++ demo_test_file.py
@@ -0,0 +1,12 @@
+# Test file version 1
+def function_a():
+    print("This is function A")
+    return "a"
+
+def function_b():
+    print("This is function B")
+    return "b"
+
+class TestClass:
+    def method_one(self):
+        return "method one"
