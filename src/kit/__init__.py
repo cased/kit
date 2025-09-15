1b8d8030e331baddae9d41ea9238dd430e293e78
tnm <t@ted.io>
2025-09-14 23:25:55 -0700

    Bump version

--- src/kit/__init__.py
+++ src/kit/__init__.py
@@ -3,7 +3,7 @@ A modular toolkit for LLM-powered codebase understanding.
 """
 
 __author__ = "cased"
-__version__ = "2.0.0rc3"
+__version__ = "2.0.0rc4"
 
 from .code_searcher import CodeSearcher
 from .context_extractor import ContextExtractor
