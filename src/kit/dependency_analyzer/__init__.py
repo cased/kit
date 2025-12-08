28ded146e11daa645c3da81dd0ce687909f419e2
tnm <t@ted.io>
2025-12-08 15:26:49 -0800

    Remove unnecessary export from __init__.py

--- src/kit/dependency_analyzer/__init__.py
+++ src/kit/dependency_analyzer/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
 from .dependency_analyzer import DependencyAnalyzer
-from .javascript_dependency_analyzer import JavaScriptDependencyAnalyzer
 
-__all__ = ["DependencyAnalyzer", "JavaScriptDependencyAnalyzer"]
+__all__ = ["DependencyAnalyzer"]
