3386b2ec64beaf12095c468fb78ceddbd5f50b81
Ted Nyman <t@ted.io>
2025-12-26 16:10:26 -0800

    Fix arrow function detection in JavaScript/TypeScript symbol extraction (#169)
    The tree-sitter query files were missing patterns to match arrow functions.
    The key issue was that const/let use lexical_declaration (not variable_declaration).
    
    - Update JavaScript tags.scm to detect arrow functions, function expressions
    - Update TypeScript tags.scm to detect non-exported arrow functions
    - Add comprehensive test coverage for arrow function detection
    - Update golden test expectations
    
    Fixes #168

--- src/kit/queries/typescript/tags.scm
+++ src/kit/queries/typescript/tags.scm
@@ -1,78 +1,116 @@
 ;; tags.scm for TypeScript symbol extraction
 
-; functions
+; Function declarations
 (function_declaration
   name: (identifier) @name @definition.function)
 
-; classes (with optional modifiers like export)
+; Class declarations (with optional modifiers like export)
 (class_declaration
   name: (type_identifier) @name @definition.class)
 
-; interfaces
+; Interface declarations
 (interface_declaration
   name: (type_identifier) @name @definition.interface)
 
-; enums
+; Enum declarations
 (enum_declaration
   name: (identifier) @name @definition.enum)
 
-; Class methods (implementations, explicitly within class_body)
+; Class methods
 (class_body
   (method_definition
     name: (property_identifier) @name @definition.method))
 
-; Static Class methods (implementations, explicitly within class_body)
-(class_body
-  (method_definition
-    "static"
-    name: (property_identifier) @name @definition.method))
+; Arrow functions assigned to const/let (lexical_declaration)
+(lexical_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: (arrow_function)) @definition.function)
 
-; Exported function (variable declarator with function)
+; Arrow functions assigned to var (variable_declaration)
+(variable_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: (arrow_function)) @definition.function)
+
+; Regular functions assigned to const/let
+(lexical_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: (function_expression)) @definition.function)
+
+; Regular functions assigned to var
+(variable_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: (function_expression)) @definition.function)
+
+; Exported arrow functions (export const/let)
+(export_statement
+  declaration: (lexical_declaration
+    (variable_declarator
+      name: (identifier) @name
+      value: (arrow_function)) @definition.function))
+
+; Exported arrow functions (export var)
 (export_statement
   declaration: (variable_declaration
     (variable_declarator
       name: (identifier) @name
-      value: (function_expression)
-    ) @definition.function
-  ))
+      value: (arrow_function)) @definition.function))
 
-; Exported function (variable declarator with arrow function)
+; Exported function expressions (const/let)
+(export_statement
+  declaration: (lexical_declaration
+    (variable_declarator
+      name: (identifier) @name
+      value: (function_expression)) @definition.function))
+
+; Exported function expressions (var)
 (export_statement
   declaration: (variable_declaration
     (variable_declarator
       name: (identifier) @name
-      value: (arrow_function)
-    ) @definition.function
-  ))
+      value: (function_expression)) @definition.function))
 
-; Exported class
+; Exported function declarations
+(export_statement
+  declaration: (function_declaration
+    name: (identifier) @name @definition.function))
+
+; Exported class declarations
 (export_statement
   declaration: (class_declaration
-    name: (type_identifier) @name @definition.class
-  ))
+    name: (type_identifier) @name @definition.class))
 
-; Exported interface
+; Exported interface declarations
 (export_statement
   declaration: (interface_declaration
-    name: (type_identifier) @name @definition.interface
-  ))
+    name: (type_identifier) @name @definition.interface))
 
-; Exported enum
+; Exported enum declarations
 (export_statement
   declaration: (enum_declaration
-    name: (identifier) @name @definition.enum
-  ))
+    name: (identifier) @name @definition.enum))
 
-; Type alias
+; Type alias declarations
 (type_alias_declaration
-    name: (type_identifier) @name @definition.type)
+  name: (type_identifier) @name @definition.type)
 
-; Exported type alias
+; Exported type alias declarations
 (export_statement
-    declaration: (type_alias_declaration
-        name: (type_identifier) @name @definition.type
-    ))
+  declaration: (type_alias_declaration
+    name: (type_identifier) @name @definition.type))
 
-; Namespace (try internal_module)
+; Namespace (internal_module)
 (internal_module
   name: (identifier) @name @definition.namespace)
+
+; Generator functions
+(generator_function_declaration
+  name: (identifier) @name @definition.function)
+
+; Exported generator functions
+(export_statement
+  declaration: (generator_function_declaration
+    name: (identifier) @name @definition.function))
